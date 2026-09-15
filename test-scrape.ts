77abea5087f1ecebc45f3cada695773d4f13bf2d
Kyle Beggan <beggank@gmail.com>
2026-09-15 18:35:09 -0400

    Remove test-scrape.ts

--- test-scrape.ts
+++
@@ -1,8 +0,0 @@
-import { scrapeRecipeFromUrl } from './src/app/actions/scrapeRecipe.ts';
-
-async function test() {
-  const result = await scrapeRecipeFromUrl('https://www.savoryonline.com/recipes/tuna-salad-seaweed-snacks/');
-  console.log(JSON.stringify(result, null, 2));
-}
-
-test();
