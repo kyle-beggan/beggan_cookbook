4c2db67bba0a662b617785228ec07f4bfc50b13f
Kyle Beggan <beggank@gmail.com>
2026-09-15 21:08:20 -0400

    fix auth callback redirect host

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -11,11 +11,14 @@ export async function GET(request: Request) {
     const { error } = await supabase.auth.exchangeCodeForSession(code)
     if (!error) {
       const forwardedHost = request.headers.get('x-forwarded-host')
+      const host = request.headers.get('host')
+      const resolvedHost = forwardedHost || host
       const isLocalEnv = process.env.NODE_ENV === 'development'
+      
       if (isLocalEnv) {
         return NextResponse.redirect(`${origin}${next}`)
-      } else if (forwardedHost) {
-        return NextResponse.redirect(`https://${forwardedHost}${next}`)
+      } else if (resolvedHost) {
+        return NextResponse.redirect(`https://${resolvedHost}${next}`)
       } else {
         return NextResponse.redirect(`${origin}${next}`)
       }
